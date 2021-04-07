e0dd15c4c478cc2379589dc6a7ecfee19f128794
Maria dos Anjos <mariadosanjos.108@gmail.com>
2021-04-07 10:04:09 -0300

    Arquivos do conversor

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Conversor de moedas
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/mariadosanjos/pen/bGgGabE](https://codepen.io/mariadosanjos/pen/bGgGabE).
+
+O objetivo deste projeto é realizar a conversão de libras esterlinas em reais.
---
+++ conversor/script.js
@@ -0,0 +1,14 @@
+function init(){
+    var librasEsterlinas = parseFloat(prompt("Quantas libras esterlinas você quer converter?"))
+var valorEmReal = (librasEsterlinas * 7.62).toFixed(2)
+alert(`Você ficará com ${valorEmReal} reais`)
+}
+
+
+//Revisão
+//variáveis var int - float - string
+//alert - parseInt - parseFloat - prompt
+//operações + somar * multiplicar
+//comentário final
+
+//Quando for formar uma string que possui dados variáveis, utilizar template string
